732b73d487f5a093ff43deb65d188145ae3008b1
Álex Maciel <aweasley.gina@gmail.com>
2025-04-17 23:37:38 -0300

    fazendo ajustes nas configuracoes do projeto

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
-import characterRoutes from './routes/characterRoutes';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import characterRoutes from './routes/characterRoutes'; // Importando as rotas
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// Usando o router de personagens
 app.use('/characters', characterRoutes);
 
-
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send("Naruto API is running 🌀");
 });
 
 mongoose
-  .connect(process.env.MONGO_URI || "")
+  .connect(process.env.MONGO_URI || '')
   .then(() => {
-    console.log("✅ MongoDB conectado");
+    console.log('✅ MongoDB conectado');
     app.listen(PORT, () => {
       console.log(`🚀 Servidor rodando na porta ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error("Erro ao conectar ao MongoDB:", err);
+    console.error('Erro ao conectar ao MongoDB:', err);
   });

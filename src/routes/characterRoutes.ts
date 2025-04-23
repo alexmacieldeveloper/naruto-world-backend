db92b7a45e4630329b35ec26c8473a023720c6e0
Álex Maciel <aweasley.gina@gmail.com>
2025-04-22 22:18:22 -0300

    criando rota que busca personagem por id

--- src/routes/characterRoutes.ts
+++ src/routes/characterRoutes.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { Character } from '../models/Character';
 
 const router = express.Router();
 
 // GET /characters – listar todos
-router.get('/', async (req, res) => {
+router.get('/', async (req: Request, res: Response) => {
   try {
     const characters = await Character.find();
     res.json(characters);
@@ -14,13 +14,28 @@ router.get('/', async (req, res) => {
 });
 
 // POST /characters
-router.post('/', async (req, res) => {
+router.post('/', async (req: Request, res: Response) => {
   try {
     const character = new Character(req.body);
     const saved = await character.save();
     res.status(201).json(saved);
   } catch (err) {
     res.status(400).json({ message: 'Erro ao criar personagem' });
+  }
+});
+
+// GET /characters/:id – buscar personagem por ID
+router.get('/:id', async (req: Request<{ id: string }>, res: Response) => {
+  try {
+    const character = await Character.findById(req.params.id);
+
+    if (!character) {
+      return res.status(404).json({ message: 'Personagem não encontrado' });
+    }
+
+    res.json(character);
+  } catch (err) {
+    res.status(500).json({ message: 'Erro ao buscar personagem por ID' });
   }
 });
 
